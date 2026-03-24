b43dde9f0d6b4a73fc5ca5074de4900461b36abb
Janrey Flores <janrey.flores@nmsc.edu.ph>
2026-03-24 11:49:25 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
+import { AuthProvider } from "./app/context/AuthContext";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
-  createRoot(rootElement).render(<App />);
+  createRoot(rootElement).render(
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
 }
